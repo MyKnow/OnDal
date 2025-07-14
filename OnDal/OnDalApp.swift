10f6fef255366c436e5b50e7312d040da22c48c4
MyKnow <myknow000@gmail.com>
2025-07-14 17:09:52 +0900

    Initial Commit

---
+++ OnDal/OnDalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnDalApp.swift
+//  OnDal
+//
+//  Created by MyKnow on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct OnDalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
